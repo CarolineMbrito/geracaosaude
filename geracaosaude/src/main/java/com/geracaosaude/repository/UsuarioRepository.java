0590727f26005055765544925a2cc5a267836ce2
carolmb <carolinembrito@hotmail.com>
2023-03-02 16:35:49 -0300

    adicionando novas camadas de segurança

--- geracaosaude/src/main/java/com/geracaosaude/repository/UsuarioRepository.java
+++ geracaosaude/src/main/java/com/geracaosaude/repository/UsuarioRepository.java
@@ -1,2 +1,14 @@
-package com.geracaosaude.repository;public interface UsuarioRepository {
+package com.geracaosaude.repository;
+import com.geracaosaude.model.Usuario;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
+    public Optional<Usuario> findByUsuario(@Param("usuario") String usuario);
+
+    public List<Usuario> findAllByNomeContainingIgnoreCase(@Param("nome") String nome);
 }
